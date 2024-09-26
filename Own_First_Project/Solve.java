6e148f6ce49ea4e72b99d1ed2eb44623a481f012
unknown <zaforchishti@gmail.com>
2024-09-26 17:05:20 +0600

    Implementing my own project

---
+++ Own_First_Project/Solve.java
@@ -0,0 +1,8 @@
+package Own_First_Project;
+
+public class Solve {
+    public static void main(String[] args) {
+        System.out.println("This is a project of my own and i will try to create a project that is pretty unique and I can make it big too");
+    }
+}
+
